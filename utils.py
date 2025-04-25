b158eb7bc9dc636f1f297755b21a3d700ef0d01e
JuNymphea <zj153@georgetown.edu>
2025-04-25 11:57:14 -0400

    data analysis

--- utils.py
+++ utils.py
@@ -18,6 +18,10 @@ def depth_distribution(data, output_name, title):
         x = sorted(map(int, data.keys()))
         y = [data[str(k)] for k in x]
 
+        y_total = sum(y)
+        print(sum(y))
+        y = [v / y_total for v in y]
+
         x_new = np.linspace(min(x), max(x), 500)
         spl = make_interp_spline(x, y, k=3)
         y_smooth = spl(x_new)
@@ -28,7 +32,7 @@ def depth_distribution(data, output_name, title):
 
     # plt.title(title)
     plt.xlabel(title)
-    plt.ylabel('Count')
+    plt.ylabel('Percentage')
     plt.xlim(0, 40) 
     plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
     ax = plt.gca()
@@ -52,6 +56,10 @@ def leaves_distribution(data, output_name, title):
         x = sorted(map(int, data.keys()))
         y = [data[str(k)] for k in x]
 
+        y_total = sum(y)
+        print(sum(y))
+        y = [v / y_total for v in y]
+
         x_new = np.linspace(min(x), max(x), 500)
         spl = make_interp_spline(x, y, k=3)
         y_smooth = spl(x_new)
@@ -62,7 +70,7 @@ def leaves_distribution(data, output_name, title):
 
     # plt.title(title)
     plt.xlabel(title)
-    plt.ylabel('Count')
+    plt.ylabel('Percentage')
     plt.xlim(0, 200) 
     plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
     ax = plt.gca()
@@ -86,6 +94,10 @@ def symmetry_distribution(data, output_name, title):
         x = sorted(map(float, data.keys()))
         y = [data[str(k)] for k in x]
 
+        y_total = sum(y)
+        print(sum(y))
+        y = [v / y_total for v in y]
+
         x_new = np.linspace(min(x), max(x), 500)
         spl = make_interp_spline(x, y, k=3)
         y_smooth = spl(x_new)
@@ -96,8 +108,47 @@ def symmetry_distribution(data, output_name, title):
 
     # plt.title(title)
     plt.xlabel(title)
-    plt.ylabel('Count')
+    plt.ylabel('Percentage')
     # plt.xlim(0, 15) 
+    plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=4, frameon=False)
+    plt.tight_layout()
+    plt.savefig(output_name)
+
+def branch_distribution(data, output_name, title):
+    plt.figure(figsize=(12, 6))
+    colors = plt.get_cmap("tab10")
+
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['axes.titlesize'] = 18
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+
+    for idx, (label, data) in enumerate(data.items()):
+        x = sorted(map(float, data.keys()))
+        y = [data[str(k)] for k in x]
+
+        y_total = sum(y)
+        print(sum(y))
+        y = [v / y_total for v in y]
+
+        x_new = np.linspace(min(x), max(x), 500)
+        spl = make_interp_spline(x, y, k=3)
+        y_smooth = spl(x_new)
+        y_smoothed = gaussian_filter1d(y_smooth, sigma=3)
+        y_smoothed = y_smoothed / np.sum(y_smoothed)
+
+        plt.plot(x_new, y_smoothed, label=label, color=colors(idx))
+        plt.fill_between(x_new, y_smoothed, alpha=0.2)
+
+    # plt.title(title)
+    plt.xlabel(title)
+    plt.ylabel('Percentage')
+    plt.xlim(0, 15) 
     plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
     ax = plt.gca()
     ax.spines['top'].set_visible(False)
@@ -118,30 +169,40 @@ with open('features/mixed.json', 'r') as file:
 with open('features/dependency.json', 'r') as file:
     dependency = json.load(file)
 
-depth = {
-    "nested": nested['depth'],
-    "constituency": constituency['depth'],
-    "mixed": mixed['depth'],
-    "dependency": dependency['depth']
+# depth = {
+#     "nested": nested['depth'],
+#     "constituency": constituency['depth'],
+#     "mixed": mixed['depth'],
+#     "dependency": dependency['depth']
+# }
+
+# depth_distribution(depth, "imgs/depth.png", "Depth")
+
+# leaves = {
+#     "nested": nested['leaves'],
+#     "constituency": constituency['leaves'],
+#     "mixed": mixed['leaves'],
+#     "dependency": dependency['leaves']
+# }
+
+# leaves_distribution(leaves, "imgs/leaves.png", "Leaves")
+
+# symmetry = {
+#     "nested": nested['symmetry'],
+#     "constituency": constituency['symmetry'],
+#     "mixed": mixed['symmetry'],
+#     "dependency": dependency['symmetry']
+# }
+
+# symmetry_distribution(symmetry, "imgs/symmetry.png", "Symmetry")
+
+branching = {
+    "nested": nested['branching']['avg'],
+    "constituency": constituency['branching']['avg'],
+    "mixed": mixed['branching']['avg'],
+    "dependency": dependency['branching']['avg']
 }
 
-depth_distribution(depth, "imgs/depth.png", "Depth")
+branch_distribution(branching, "imgs/branching.png", "Branch")
 
-leaves = {
-    "nested": nested['leaves'],
-    "constituency": constituency['leaves'],
-    "mixed": mixed['leaves'],
-    "dependency": dependency['leaves']
-}
-
-leaves_distribution(leaves, "imgs/leaves.png", "Leaves")
-
-symmetry = {
-    "nested": nested['symmetry'],
-    "constituency": constituency['symmetry'],
-    "mixed": mixed['symmetry'],
-    "dependency": dependency['symmetry']
-}
-
-symmetry_distribution(symmetry, "imgs/symmetry.png", "Symmetry")
 
